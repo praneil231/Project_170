5b96d287e092a1085ea7d77df9464a10912168d4
praneil231 <84107249+praneil231@users.noreply.github.com>
2024-02-25 18:06:28 -0500

    Add files via upload

---
+++ Class_170.py
@@ -0,0 +1,35 @@
+class History_in_space():
+    def year_2022(self):
+        print("\n","Events happened in space 2022","\n")
+        print("On 29 March 2022, the Long March 6A rocket performed its maiden launch, successfully reaching orbit. On 29 April 2022, the Angara 1.2 rocket had its maiden launch, successfully reaching orbit. On 2 May 2022, Rocket Lab attempted first mid-air helicopter capture of the first stage of their Electron rocket.")
+        
+    def year_2023(self):
+        print("\n","Event occurred in the feild of space in 2022","\n")
+        
+class History_in_medical():
+    def year_2022(self):
+        print("\n","Event occurred in the feild of medical 2022","\n")
+        print("In general, a medical history includes an inquiry into the patient's medical history, past surgical history, family medical history, social history, allergies, and medications the patient is taking or may have recently stopped taking.")
+    
+    def year_2023(self):
+            print("\n","event occurred in the feild of medical in 2022","\n")
+            
+class History_in_sports():
+    def year_2022(self):
+        print("\n","Events happend in sports 2022","\n")
+        print("2022 in sports describes the year's events in world sports. The main events for this year were the 2022 Winter Olympic Games in Beijing and the 2022 FIFA World Cup in Qatar.")
+        
+    def year_2023(self):
+            print("\n","event occurred in the feild of sports in 2022","\n")
+            
+space = History_in_space()
+space.year_2022()
+space.year_2023()
+
+medical = History_in_medical()
+medical.year_2022()
+medical.year_2023()
+
+sports = History_in_sports()
+sports.year_2022()
+sports.year_2023()
